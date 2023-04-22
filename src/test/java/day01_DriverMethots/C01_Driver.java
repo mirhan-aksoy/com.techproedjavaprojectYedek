29f678c3f52331d439b5e15b6a29327334771703
mirhan-aksoy <mirhanaksoy63@gmail.com>
2023-04-22 09:34:20 +0300

    Merge remote-tracking branch 'github/mirhan' into mirhan

--- src/test/java/day01_DriverMethots/C01_Driver.java
+++ src/test/java/day01_DriverMethots/C01_Driver.java
@@ -2,17 +2,20 @@ package day01_DriverMethots;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class C01_Driver {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
-        System.setProperty("webdriver.http.factory", "jdk-http-client");
+       // System.setProperty("webdriver.http.factory", "jdk-http-client");
         //System.setProperty java uygulamalarında sistem
         // özelliklerini ayarlamak için kullanılır
         //Bu method ile class'ımıza webdriver'in fiziki yerini belirtiriz
         //Key, value olarak önce isim sonra driver'imizin yolunu belirtiriz
-        WebDriver driver = new ChromeDriver();
+        System.setProperty("webdriver.http.factory", "jdk-http-client");
+        WebDriver driver = new ChromeDriver(new ChromeOptions().addArguments("--remote-allow-origins=*"));
+       // WebDriver driver = new ChromeDriver();
 
         driver.get("https://www.amazon.com");// get() methodu ile adresini verdiğim sayfaya gider
 
@@ -21,6 +24,18 @@ public class C01_Driver {
         System.out.println("sayfa url i "+driver.getCurrentUrl());//içinde bulundugumuz sayfanın url ini getirir
 
        // System.out.println(driver.getPageSource());// bulundugum sayfanın html kaynak kodlarını verir
+        Thread.sleep(2000);
+
+        driver.get("https://www.youtube.com");
+        Thread.sleep(2000);
+
+        System.out.println("sayfa url i"+driver.getCurrentUrl());
+        Thread.sleep(2000);
+
+        System.out.println("sayfa başlığı"+driver.getTitle());
+        Thread.sleep(2000);
+
+        System.out.println("sayfanın hash kodu"+driver.hashCode());
 
 
     }
---
+++ src/test/java/day01_DriverMethots/Tekrar.java
@@ -0,0 +1,27 @@
+package day01_DriverMethots;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class Tekrar {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        System.setProperty("chromeDriver","src/resources/driver/chromedriver.exe");
+        WebDriver driver = new ChromeDriver(new ChromeOptions().addArguments("--remote-allow-origins=*"));
+
+        driver.get("https://www.youtube.com");
+        Thread.sleep(2000);
+
+        System.out.println("sayfa url i"+driver.getCurrentUrl());
+        Thread.sleep(2000);
+
+        System.out.println("sayfa başlığı"+driver.getTitle());
+        Thread.sleep(2000);
+
+        System.out.println("sayfanın hash kodu"+driver.hashCode());
+
+
+    }
+}
