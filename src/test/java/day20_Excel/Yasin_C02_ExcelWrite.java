2b7e88fbc9d9cdb37c2bb1b6f153b7fdf10860df
mirhan-aksoy <mirhanaksoy63@gmail.com>
2023-04-24 22:50:43 +0300

    Merge remote-tracking branch 'github/mirhan' into mirhan

--- src/test/java/day20_Excel/Yasin_C02_ExcelWrite.java
+++ src/test/java/day20_Excel/Yasin_C02_ExcelWrite.java
@@ -13,16 +13,19 @@ import java.io.IOException;
 public class Yasin_C02_ExcelWrite {
     /*
     Bir "NÜFUS" sütunu oluşturup başkent nüfuslarını excel doyasına yazınız:
+
     (D.C: 712816, Paris: 2161000, London: 8982000, Ankara: 5663000 ...)
      */
 
     @Test
     public void excelWriteTest() throws IOException {
         //WorkBook ==> Sheet ==> Row ==> Cell ==> Yaz ==> Kaydet
+
         FileInputStream fileInputStream = new FileInputStream("src/test/java/resourse/Capitals (2).xlsx");
         Workbook workbook = WorkbookFactory.create(fileInputStream);
 
         Sheet sheet1 = workbook.getSheet("Sheet1");
+
         //1. Satır 3. sütuna yeni hücre oluştur:
         Cell yeniHucreNufus = sheet1.getRow(0).createCell(2);
         yeniHucreNufus.setCellValue("NÜFUS");
